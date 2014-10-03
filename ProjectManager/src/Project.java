32777293f492ede9b1a530decbe65dd936542acb
BasketaksSjargong <tusslejargon@gmail.com>
2014-10-03 03:11:51 +0200

    Project class compiles

--- ProjectManager/src/Project.java
+++ ProjectManager/src/Project.java
@@ -1,4 +1,31 @@
 public class Project {
 	// This class is the graph implementation of
-	// a project.
+	// a project. It contains a ProjectReader for
+	// generating the graph structure.
+	//
+	// The generated tasks are stored in Task[] projectTasks
+	// ordered after ID from low to high.
+	
+	// Field declarations
+	private ProjectReader reader;
+	private Task[] projectTasks;
+
+	// Constructor
+	public Project(String fileName){
+		this.reader = new ProjectReader(fileName);
+	}
+	
+	// Public methods	
+	public void initializeProject(){
+		reader.generateProject();
+		projectTasks = reader.getProjectTasks();
+	}
+
+	public void printProjectTasks(){
+		// Prints any relevant information about the
+		// individual tasks.
+		for(Task t : projectTasks){
+			t.printTask();
+		}
+	}
 }
